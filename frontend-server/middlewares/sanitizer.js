871325dfbf84131d83fdb192b0aafadd4ecc6379
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2019-05-11 18:09:00 +0200

    Fix double-sanitization issue
    When a user logs in with a special character in his username, he
    recieves a sanitized username. However, when the user tries to for
    example create a room, his sanitized username is sanitized again,
    causing a authentication error. This commit fixes this issue, by
    first desanitizing the payload before sanitizing it, preventing a
    re-sanitization

--- frontend-server/middlewares/sanitizer.js
+++ frontend-server/middlewares/sanitizer.js
@@ -1,10 +1,14 @@
-let { sanitizeHTMLString } = require("../../helpers/stringSanitization");
+let {
+    sanitizeHTMLString,
+    desanitizeHTMLString
+} = require("../../helpers/stringSanitization");
 
 const middleware = prop => {
     return (req, res, next) => {
         for (const key of Object.keys(req[prop])) {
             const value = req[prop][key];
-            req[prop][key] = sanitizeHTMLString(value);
+            // Desanitize before sanitizing to prevent double-sanitization
+            req[prop][key] = sanitizeHTMLString(desanitizeHTMLString(value));
         }
 
         next();
