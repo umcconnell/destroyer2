9733c70351403dbebbaca1d65459067a4ea71079
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2019-05-11 17:58:18 +0200

    Add desanitizeHTMLString method

--- helpers/stringSanitization.js
+++ helpers/stringSanitization.js
@@ -1,15 +1,33 @@
+const mapping = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&#x27;",
+    "/": "&#x2F;"
+};
+
 exports.sanitizeHTMLString = function(input) {
     if (input === "" || typeof input !== "string") return input;
-    const mapping = {
-        "&": "&amp;",
-        "<": "&lt;",
-        ">": "&gt;",
-        '"': "&quot;",
-        "'": "&#x27;",
-        "/": "&#x2F;"
-    };
     return input.replace(/[&<>"'\/]/g, chara => mapping[chara]);
 };
+
+exports.desanitizeHTMLString = function(input) {
+    if (input === "" || typeof input !== "string") return input;
+
+    return input.replace(
+        new RegExp(
+            "(" +
+                Object.values(mapping)
+                    .map(val => `(${val})`)
+                    .join("|") +
+                ")",
+            "g"
+        ),
+        group => Object.entries(mapping).find(entry => (entry[1] = group))[0]
+    );
+};
+
 exports.cleanUTFString = function(input) {
     let output = "";
     for (let i = 0; i < input.length; i++) {
