152ac4423c10b980fbd6089067f4967c9acea41e
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2019-05-15 19:15:37 +0200

    Add noColor format to winston
    This prevents ANSI escape codes from beeing displayed, e.g. in the
    logs.

--- helpers/logger.js
+++ helpers/logger.js
@@ -1,6 +1,19 @@
 let winston = require("winston");
 let environment = process.env.NODE_ENV || "production";
 
+let noColor = winston.format(info => {
+    // Matches ANSI escape codes
+    // taken from https://github.com/chalk/ansi-regex
+    const pattern = [
+        "[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)",
+        "(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-ntqry=><~]))"
+    ].join("|");
+
+    info.message = info.message.replace(pattern, "");
+
+    return info;
+});
+
 let config = {
     console: {
         format: winston.format.combine(
