c6fe370f9b42e408c560249b99885a8b329a5a61
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2019-05-15 18:34:47 +0200

    Add error log file output to logger

--- helpers/logger.js
+++ helpers/logger.js
@@ -7,13 +7,22 @@ let config = {
             winston.format.colorize(),
             winston.format.simple()
         )
+    },
+    errorLog: {
+        level: "error",
+        filename: "logs/error.log",
+        maxsize: 5242880, // 5MB
+        maxFiles: 5
     }
 };
 
 let logger = winston.createLogger({
     level: environment === "production" ? "info" : "debug",
     format: winston.format.simple(),
-    transports: [new winston.transports.Console(config.console)],
+    transports: [
+        new winston.transports.Console(config.console),
+        new winston.transports.File(config.errorLog)
+    ],
     exitOnError: false
 });
 
