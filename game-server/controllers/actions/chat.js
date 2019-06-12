0eec4595e9a704eab5c1a8ba966208c2e39bdeb3
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2019-06-12 18:35:07 +0200

    Delete chat.js action

--- game-server/controllers/actions/chat.js
+++
@@ -1,9 +0,0 @@
-let root = require("app-root-path");
-let logger = require(`${root}/helpers/logger`);
-
-let { messageSchemas } = require(`${root}/models/schemas`);
-
-module.exports = function(msg, ws, wss, room) {
-    logger.debug(`Recieved chat msg: ${msg}`);
-    room.players.forEach(player => player.send(messageSchemas("chat", msg)));
-};
