9421c06b08eb0b00bc16b83603d781d54d0c0070
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2020-04-07 23:27:50 +0200

    Add abortHandshake websocket util

--- helpers/websocket.js
+++ helpers/websocket.js
@@ -1,9 +1,37 @@
+const { STATUS_CODES } = require("http");
 let root = require("app-root-path");
 let { messageSchemas } = require(`${root}/models/schemas`);
 
-exports.heartbeat = function() {
+exports.heartbeat = function () {
     this.isAlive = true;
 };
+
 exports.noop = () => {};
 
-exports.rejectWs = ws => ws.send(messageSchemas("error", "invalid input"));
+exports.rejectWs = (ws) => ws.send(messageSchemas("error", "invalid input"));
+
+// Taken from the ws library
+// https://github.com/websockets/ws/blob/08c6c8ba70404818f7f4bc23eb5fd0bf9c94c039/lib/websocket-server.js#L384-L406
+exports.abortHandshake = (socket, code, message, headers) => {
+    if (socket.writable) {
+        message = message || STATUS_CODES[code];
+        headers = {
+            Connection: "close",
+            "Content-type": "text/html",
+            "Content-Length": Buffer.byteLength(message),
+            ...headers
+        };
+
+        socket.write(
+            `HTTP/1.1 ${code} ${STATUS_CODES[code]}\r\n` +
+                Object.keys(headers)
+                    .map((h) => `${h}: ${headers[h]}`)
+                    .join("\r\n") +
+                "\r\n\r\n" +
+                message
+        );
+    }
+
+    socket.removeListener("error", socketOnError);
+    socket.destroy();
+};
