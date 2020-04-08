ab38a6c2d30b3be8612a6360dc5ca0a4478b41d8
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2020-04-08 19:08:39 +0200

    Rewrite ws setupConnection

--- game-server/controllers/setupConnection.js
+++ game-server/controllers/setupConnection.js
@@ -2,7 +2,7 @@ let root = require("app-root-path");
 let logger = require(`${root}/helpers/logger`);
 
 let Rooms = require(`${root}/models/rooms`);
-let Users = require(`${root}/models/users`);
+let { ServerError } = require(`${root}/models/schemas`);
 let { roomInfo } = require(`${root}/models/schemas`);
 
 let { heartbeat } = require(`${root}/helpers/websocket`);
@@ -12,17 +12,46 @@ let onclose = require("./onclose");
 let sendTurn = require("./actions/turn");
 let join = require("./actions/join");
 
-function setupConnection(ws, params, wss, ROOMS) {
+exports.addUserToRoom = async function addUserToRoom(userId, roomId) {
+    try {
+        let room = await Rooms.get(roomId);
+        if (!room) throw new ServerError(404, "room not found");
+
+        let players = room.players.split(",").filter((el) => !!el);
+
+        if (players.length >= 2) throw new ServerError(403, "room full");
+        else if (players.includes(userId)) {
+            throw new ServerError(403, "player already in room");
+        }
+
+        await Rooms.update(
+            roomId,
+            "players",
+            players.concat([userId]).join(",")
+        );
+
+        let full = players.length + 1 === 2;
+        if (full) await Rooms.close(roomId);
+    } catch (e) {
+        if (e instanceof ServerError) throw e;
+        // Catch internal server errors such as problems with the DB
+        else throw new ServerError();
+    }
+};
+
+exports.setupConnection = async function setupConnection(
+    ws,
+    user,
+    roomId,
+    wss,
+    ROOMS
+) {
     // Save user info to ws connection object
-    ws.userId = params.t;
-    ws.roomId = params.r;
+    ws.userName = user.userName;
+    ws.userId = user.userId;
+    ws.roomId = roomId;
     ws.isAlive = true;
     ws.placed = false;
-    Users.get(ws.userId)
-        .then(name => (ws.userName = name))
-        .catch(err =>
-            logger.error(`Internal Server Error: ${err.stack || err}`)
-        );
 
     if (ws.roomId in ROOMS) ROOMS[ws.roomId].players.push(ws);
     else ROOMS[ws.roomId] = new roomInfo(ws);
@@ -31,24 +60,23 @@ function setupConnection(ws, params, wss, ROOMS) {
 
     // Turn might be reset after gameOver but room still exists
     if (!room.turn) {
-        Rooms.getVal(ws.roomId, "turn")
-            .then(turn => (room.turn = turn ? turn : ws.userId))
-            .then(Rooms.delVal(ws.roomId, "turn"))
-            .catch(err =>
-                logger.error(`Internal Server Error: ${err.stack || err}`)
-            )
-            .then(() => sendTurn(null, ws, wss, room));
-    } else {
-        sendTurn(null, ws, wss, room);
+        try {
+            turn = await Rooms.getVal(ws.roomId, "turn");
+            room.turn = turn ? turn : ws.userId;
+
+            await Rooms.delVal(ws.roomId, "turn");
+        } catch (e) {
+            logger.error(`Internal Server Error: ${err.stack || err}`);
+        }
     }
+
+    sendTurn(null, ws, wss, room);
 
     join(null, ws, wss, room);
 
     ws.on("pong", heartbeat);
 
-    ws.on("message", msg => onmessage(msg, ws, wss, room));
+    ws.on("message", (msg) => onmessage(msg, ws, wss, room));
 
     ws.on("close", () => onclose(null, ws, wss, room, ROOMS));
-}
-
-module.exports = setupConnection;
+};
