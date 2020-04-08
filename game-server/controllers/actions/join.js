894afd6bcf1a2e7ceb4f96228d8d72f54c28940d
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2020-04-08 20:24:33 +0200

    Rewrite ws join handler

--- game-server/controllers/actions/join.js
+++ game-server/controllers/actions/join.js
@@ -5,113 +5,109 @@ let Rooms = require(`${root}/models/rooms`);
 let { messageSchemas } = require(`${root}/models/schemas`);
 let { countShips } = require(`${root}/helpers/game`);
 
-module.exports = function(msg, ws, wss, room) {
-    let other = room.players.find(player => player.userId !== ws.userId);
+async function seaAlreadyPlaced(ws, room, dbRoom, other, seas) {
+    let mySea = dbRoom[`sea-${ws.userId}`],
+        enemySea =
+            seas.find((sea) => sea !== ws.userId) &&
+            dbRoom[`sea-${seas.find((sea) => sea !== ws.userId)}`];
 
-    return Rooms.get(ws.roomId)
-        .then(Room => {
-            let seas = Object.keys(Room)
-                .filter(key => key.startsWith("sea-"))
-                .map(sea => sea.slice(4));
+    // let user skip shippicker interface
+    let msg = mySea;
+    // Add enemy field if placed
+    if (enemySea) {
+        msg = msg.concat(";").concat(
+            enemySea
+                .split("")
+                .map((field) => (field === "1" || field === "2" ? field : "0"))
+                .join("")
+        );
+    }
 
-            // Case: sea already placed
-            if (seas.includes(ws.userId)) {
-                let mySea = Room[`sea-${ws.userId}`],
-                    enemySea =
-                        seas.find(sea => sea !== ws.userId) &&
-                        Room[`sea-${seas.find(sea => sea !== ws.userId)}`];
+    ws.send(messageSchemas("alreadyPlaced", msg));
 
-                // let user skip shippicker interface
-                let msg = mySea;
-                // Add enemy field if placed
-                if (enemySea) {
-                    msg = msg.concat(";").concat(
-                        enemySea
-                            .split("")
-                            .map(field =>
-                                field === "1" || field === "2" ? field : "0"
-                            )
-                            .join("")
-                    );
-                }
-
-                ws.send(messageSchemas("alreadyPlaced", msg));
-
-                // Send ships counter to player if he already played
-                // with other player
-                if (enemySea) {
-                    ws.send(
-                        messageSchemas(
-                            "count",
-                            JSON.stringify({
-                                me: countShips(mySea.split("")).reduce(
-                                    (acc, curr) => (curr === 0 ? acc : acc + 1),
-                                    0
-                                ),
-                                enemy:
-                                    enemySea &&
-                                    countShips(enemySea.split("")).reduce(
-                                        (acc, curr) =>
-                                            curr === 0 ? acc : acc + 1,
-                                        0
-                                    )
-                            })
+    // Send ships counter to player if he already played
+    // with other player
+    if (enemySea) {
+        ws.send(
+            messageSchemas(
+                "count",
+                JSON.stringify({
+                    me: countShips(mySea.split("")).reduce(
+                        (acc, curr) => (curr === 0 ? acc : acc + 1),
+                        0
+                    ),
+                    enemy:
+                        enemySea &&
+                        countShips(enemySea.split("")).reduce(
+                            (acc, curr) => (curr === 0 ? acc : acc + 1),
+                            0
                         )
-                    );
-                }
+                })
+            )
+        );
+    }
 
-                ws.placed = true;
+    ws.placed = true;
 
-                other &&
-                    other.send(
-                        messageSchemas("join", `${ws.userName} rejoined`)
-                    );
+    if (other) other.send(messageSchemas("join", `${ws.userName} rejoined`));
 
-                // remove loader if other player waiting
-                if (
-                    room.players.length > 1 &&
-                    room.players.every(player => player.placed)
-                ) {
-                    room.ready = true;
-                    room.players.forEach(player =>
-                        player.send(
-                            messageSchemas(
-                                "ready",
-                                JSON.stringify({
-                                    msg: `${ws.userName} placed his ships`,
-                                    enemy: room.players.find(
-                                        enemy => enemy.userId !== player.userId
-                                    ).userName
-                                })
-                            )
-                        )
-                    );
-                }
-            } else {
-                // Case: player new joined, but other player waiting with placed ship
-                if (seas.length > 1) {
-                    seas.forEach(sea => Rooms.delVal(ws.roomId, `sea-${sea}`));
+    // remove loader if other player waiting
+    if (
+        room.players.length > 1 &&
+        room.players.every((player) => player.placed)
+    ) {
+        room.ready = true;
+        room.players.forEach((player) =>
+            player.send(
+                messageSchemas(
+                    "ready",
+                    JSON.stringify({
+                        msg: `${ws.userName} placed his ships`,
+                        enemy: room.players.find(
+                            (enemy) => enemy.userId !== player.userId
+                        ).userName
+                    })
+                )
+            )
+        );
+    }
+}
 
-                    other &&
-                        other.placed &&
-                        other.send(
-                            messageSchemas(
-                                "reset",
-                                `a different player joined: ${ws.userName}`
-                            )
-                        );
+async function differentPlayer(ws, room, dbRoom, other, seas) {
+    await Promise.all(seas.map((sea) => Rooms.delVal(ws.roomId, `sea-${sea}`)));
 
-                    room.players.forEach(player => (player.placed = false));
-                } else {
-                    other &&
-                        other.send(
-                            messageSchemas("join", `${ws.userName} joined`)
-                        );
-                }
+    if (other && other.placed) {
+        other.send(
+            messageSchemas("reset", `a different player joined: ${ws.userName}`)
+        );
+
+        room.turn = other.userId;
+        await Rooms.update(ws.roomId, "turn", other.userId);
+    }
+
+    room.players.forEach((player) => (player.placed = false));
+}
+
+module.exports = async function (msg, ws, wss, room) {
+    let other = room.players.find((player) => player.userId !== ws.userId);
+
+    try {
+        dbRoom = await Rooms.get(ws.roomId);
+        let seas = Object.keys(dbRoom)
+            .filter((key) => key.startsWith("sea-"))
+            .map((sea) => sea.slice(4));
+
+        if (seas.includes(ws.userId)) {
+            await seaAlreadyPlaced(ws, room, dbRoom, other, seas);
+        } else {
+            if (seas.length > 1) {
+                await differentPlayer(ws, room, dbRoom, other, seas);
+            } else if (other) {
+                other.send(messageSchemas("join", `${ws.userName} joined`));
             }
-        })
-        .catch(err => {
-            logger.error(err => `Internal Server Error: ${err.stack || err}`);
-            ws.send(messageSchemas("error", "internal server error"));
-        });
+        }
+    } catch (err) {
+        logger.error(`Internal Server Error: ${err.stack || err}`);
+        ws.send(messageSchemas("error", "internal server error"));
+    }
 };
