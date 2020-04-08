2e3cb72242636173e912f498ef615332259c2246
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2020-04-08 20:55:59 +0200

    Extract desanitize RegExp to module scope
    Previously a new dynamic regular expression was created on every
    API call. By extracting the regular expression to the module scope
    it is only computed once.

--- helpers/stringSanitization.js
+++ helpers/stringSanitization.js
@@ -7,28 +7,31 @@ const mapping = {
     "/": "&#x2F;"
 };
 
-exports.sanitizeHTMLString = function(input) {
+const desanitizeRegexp = new RegExp(
+    "(" +
+        Object.values(mapping)
+            .map((val) => `(${val})`)
+            .join("|") +
+        ")",
+    "g"
+);
+
+exports.sanitizeHTMLString = function (input) {
     if (input === "" || typeof input !== "string") return input;
-    return input.replace(/[&<>"'\/]/g, chara => mapping[chara]);
+    return input.replace(/[&<>"'\/]/g, (chara) => mapping[chara]);
 };
 
-exports.desanitizeHTMLString = function(input) {
+exports.desanitizeHTMLString = function (input) {
     if (input === "" || typeof input !== "string") return input;
 
     return input.replace(
-        new RegExp(
-            "(" +
-                Object.values(mapping)
-                    .map(val => `(${val})`)
-                    .join("|") +
-                ")",
-            "g"
-        ),
-        group => Object.entries(mapping).find(entry => (entry[1] = group))[0]
+        desanitizeRegexp,
+        (group) =>
+            Object.entries(mapping).find((entry) => (entry[1] = group))[0]
     );
 };
 
-exports.cleanUTFString = function(input) {
+exports.cleanUTFString = function (input) {
     let output = "";
     for (let i = 0; i < input.length; i++) {
         if (input.charCodeAt(i) <= 127) {
