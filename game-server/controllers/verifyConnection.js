ff6859dd6b748fa628035d8c2c50253dae92c09e
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2020-04-08 19:08:10 +0200

    Rewrite ws verifyConnection

--- game-server/controllers/verifyConnection.js
+++ game-server/controllers/verifyConnection.js
@@ -1,76 +1,28 @@
+let url = require("url");
 let root = require("app-root-path");
-let logger = require(`${root}/helpers/logger`);
 
-let Users = require(`${root}/models/users`);
-let Rooms = require(`${root}/models/rooms`);
+let { verify } = require(`${root}/helpers/auth`);
 
-function verifyConnection(clients, params) {
-    let reject = (msg, status) => {
-        // Custom error log
-        if (msg)
-            logger.error(
-                `WebSocket Error${status ? " " + status : ""}${
-                    msg ? " : " + msg : ""
-                }`
-            );
+let { ServerError } = require(`${root}/models/schemas`);
 
-        return {
-            accepted: false,
-            status: status || 500,
-            msg: msg || "server error"
-        };
-    };
+exports.verifyConnection = async function verifyConnection(clients, req) {
+    let { t: token, r: roomId } = url.parse(req.url, true).query;
 
-    if (!params.t || !params.r)
-        return reject("please specify user-token and room-token", 400);
+    if (!token || !roomId)
+        throw new ServerError(400, "please specify user-token and room-token");
+
+    let user;
+    try {
+        user = await verify(token);
+    } catch (e) {
+        throw new ServerError(401, "unauthorized");
+    }
 
     let userAlreadyConnected = Array.from(clients).find(
-        (client) => client.userId === params.t
+        (client) => client.userId === user.userId
     );
     if (userAlreadyConnected)
-        return reject("user already connected to room", 403);
+        throw new ServerError(403, "user already connected to room");
 
-    return Users.exists(params.t)
-        .then((exists) => {
-            if (!exists) throw "unauthorized";
-        })
-        .then(() =>
-            Rooms.get(params.r)
-                .then((room) => {
-                    if (!room) throw "room not found";
-                    let players = room.players
-                        .split(",")
-                        // Remove empty fields (e.g "".split(",") => [""])
-                        .filter((el) => !!el);
-
-                    if (players.includes(params.t))
-                        throw "player already in room";
-                    else if (players.length < 2) return players;
-                    else throw "room full";
-                })
-                // Add player to players list
-                .then((players) =>
-                    Rooms.update(
-                        params.r,
-                        "players",
-                        players.concat([params.t]).join(",")
-                    )
-                        // Room is full?
-                        .then(() => players.length + 1 === 2)
-                        .catch(reject)
-                )
-                // Remove from openrooms if full
-                .then((full) =>
-                    full
-                        ? Rooms.close(params.r).catch(() => {
-                              throw "server error";
-                          })
-                        : ""
-                )
-                // Accept user
-                .then(() => ({ accepted: true }))
-        )
-        .catch(reject);
-}
-
-module.exports = verifyConnection;
+    return { user, roomId };
+};
