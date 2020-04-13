942be9c8948ae14fb64c86ee7a82eb1fc94441c2
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2020-04-13 21:16:10 +0200

    Make sure it's always some user's turn

--- game-server/controllers/actions/turn.js
+++ game-server/controllers/actions/turn.js
@@ -1,6 +1,10 @@
 let root = require("app-root-path");
 let { messageSchemas } = require(`${root}/models/schemas`);
 
-module.exports = function(msg, ws, wss, room) {
+module.exports = function (msg, ws, wss, room) {
+    if (room.players.length == 2) {
+        let turn = room.players.find((ws) => ws.userId == room.turn);
+        if (!turn) room.turn = ws.userId;
+    }
     return ws.send(messageSchemas("turn", room.turn === ws.userId));
 };
