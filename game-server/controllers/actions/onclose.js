139329d776bc0a2f095e3fa4cdf7e5a73141b777
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2020-04-13 19:13:19 +0200

    Migrate actions/onclose to async and ServerError

--- game-server/controllers/actions/onclose.js
+++ game-server/controllers/actions/onclose.js
@@ -2,60 +2,47 @@ let root = require("app-root-path");
 let logger = require(`${root}/helpers/logger`);
 
 let Rooms = require(`${root}/models/rooms`);
-let { messageSchemas } = require(`${root}/models/schemas`);
+let { ServerError, messageSchemas } = require(`${root}/models/schemas`);
 
-function closeWs(playerId, roomId) {
-    return (
-        Rooms.get(roomId)
-            .then(room => {
-                if (!room) throw new Error("room not found");
-                return (
-                    room.players
-                        .split(",")
-                        // Remove empty fields (e.g "".split(",") => [""])
-                        .filter(el => !!el)
-                );
-            })
-            // Set new players and remove player sea
-            .then(players => {
-                let newPlayers = players.filter(id => id !== playerId);
-                return Rooms.update(
-                    roomId,
-                    "players",
-                    newPlayers.join(",")
-                ).then(() => newPlayers);
-            })
-            .then(players => {
-                players.length < 2 ? Rooms.open(roomId) : "";
-            })
-    );
+async function closeWs(playerId, roomId) {
+    let room = await Rooms.get(roomId);
+
+    if (!room) throw new ServerError(404, "room not found");
+
+    // Remove empty fields (e.g "".split(",") => [""])
+    let players = room.players.split(",").filter((el) => !!el);
+
+    let newPlayers = players.filter((id) => id !== playerId);
+    await Rooms.update(roomId, "players", newPlayers.join(","));
+
+    if (newPlayers.length < 2) Rooms.open(roomId);
 }
 
-function onclose(msg, ws, wss, room, ROOMS) {
-    return closeWs(ws.userId, ws.roomId)
-        .then(() => {
-            let newPlayers = room.players.filter(
-                player => player.userId !== ws.userId
-            );
+async function onclose(msg, ws, wss, room, ROOMS) {
+    try {
+        await closeWs(ws.userId, ws.roomId);
 
-            // Delete room if empty
-            if (newPlayers.length === 0) {
-                Rooms.setVal(ws.roomId, "turn", room.turn || "");
-                delete ROOMS[ws.roomId];
-                return false;
-            } else {
-                newPlayers.forEach(player =>
-                    player.send(messageSchemas("leave", `${ws.userName} left`))
-                );
-                room.players = newPlayers;
-                room.ready = false;
-                return true;
-            }
-        })
-        .catch(err => {
-            if (err.message === "room not found") return;
-            logger.error(`Internal Server Error: ${err.stack || err}`);
-        });
+        let newPlayers = room.players.filter(
+            (player) => player.userId !== ws.userId
+        );
+
+        // Delete room if empty
+        if (newPlayers.length === 0) {
+            Rooms.setVal(ws.roomId, "turn", room.turn || "");
+            delete ROOMS[ws.roomId];
+            return false;
+        } else {
+            newPlayers.forEach((player) =>
+                player.send(messageSchemas("leave", `${ws.userName} left`))
+            );
+            room.players = newPlayers;
+            room.ready = false;
+            return true;
+        }
+    } catch (err) {
+        if (err.message === "room not found") return;
+        logger.error(`Internal Server Error: ${err.stack || err}`);
+    }
 }
 
 module.exports = onclose;
