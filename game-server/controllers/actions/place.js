e364a4aaa35fccfd22b720a72e77a746d2c877a1
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2020-04-13 19:19:43 +0200

    Move actions/place to async

--- game-server/controllers/actions/place.js
+++ game-server/controllers/actions/place.js
@@ -5,57 +5,52 @@ let Rooms = require(`${root}/models/rooms`);
 let { messageSchemas } = require(`${root}/models/schemas`);
 let { validGameField } = require(`${root}/helpers/game`);
 
-module.exports = function(msg, ws, wss, room) {
-    console.log(msg);
+module.exports = async function (msg, ws, wss, room) {
     let valid = validGameField(msg.split(""));
 
     if (!valid.valid) {
         ws.send(messageSchemas("error", valid.msg));
     } else {
-        Rooms.hasVal(ws.roomId, `sea-${ws.userId}`)
-            .then(has => {
-                if (!has) {
-                    return Rooms.update(
-                        ws.roomId,
-                        `sea-${ws.userId}`,
-                        msg
-                    ).then(() => {
-                        ws.placed = true;
-                        if (room.players.every(player => player.placed))
-                            room.ready = true;
+        try {
+            let has = await Rooms.hasVal(ws.roomId, `sea-${ws.userId}`);
+            if (!has) {
+                await Rooms.update(ws.roomId, `sea-${ws.userId}`, msg);
 
-                        let other = room.players.find(
-                            user => user.userId !== ws.userId && user.placed
-                        );
+                ws.placed = true;
+                if (room.players.every((player) => player.placed))
+                    room.ready = true;
 
-                        other &&
-                            other.send(
-                                messageSchemas(
-                                    "ready",
-                                    JSON.stringify({
-                                        msg: `${ws.userName} placed his ships`,
-                                        enemy: ws.userName
-                                    })
-                                )
-                            );
+                let other = room.players.find(
+                    (user) => user.userId !== ws.userId && user.placed
+                );
 
-                        ws.send(
-                            messageSchemas(
-                                other ? "ready" : "placed",
-                                other
-                                    ? JSON.stringify({
-                                          msg: `successfully placed ships`,
-                                          enemy: other.userName
-                                      })
-                                    : "successfully placed ships"
-                            )
-                        );
-                    });
+                if (other) {
+                    other.send(
+                        messageSchemas(
+                            "ready",
+                            JSON.stringify({
+                                msg: `${ws.userName} placed his ships`,
+                                enemy: ws.userName
+                            })
+                        )
+                    );
                 }
-            })
-            .catch(err => {
-                logger.error(`Internal Server Error: ${err.stack || err}`);
-                ws.send(messageSchemas("error", "internal server error"));
-            });
+
+                ws.send(
+                    messageSchemas(
+                        other ? "ready" : "placed",
+                        other
+                            ? JSON.stringify({
+                                  msg: `successfully placed ships`,
+                                  enemy: other.userName
+                              })
+                            : "successfully placed ships"
+                    )
+                );
+            }
+        } catch (err) {
+            logger.error(`Internal Server Error: ${err.stack || err}`);
+            ws.send(messageSchemas("error", "internal server error"));
+        }
     }
 };
