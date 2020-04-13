89c157a3bfb209cfbb231cff58b976cbca250000
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2020-04-13 20:49:02 +0200

    Move frontend apiActions to async

--- frontend-server/public/javascripts/apiActions.js
+++ frontend-server/public/javascripts/apiActions.js
@@ -15,54 +15,56 @@ async function parseOrThrow(resp) {
 
 // Main
 // GET /api/openrooms
-function openRooms(lastmodified = false) {
+async function openRooms(lastmodified = false) {
     let modifiedHeaders = lastmodified
         ? { "If-Modified-Since": lastmodified }
         : {};
 
-    return fetch("/api/openrooms", {
+    const resp = await fetch("/api/openrooms", {
         headers: { ...modifiedHeaders, ...headers }
-    }).then(async (resp) => {
-        let lastmodified = resp.headers.get("Modified-Since");
-
-        if (resp.status === 304) return { lastmodified, rooms: false };
-        let json = await resp.json();
-        if (!resp.ok) throw `${resp.status} ${resp.statusText}: ${json.error}`;
-        else return { lastmodified, rooms: json };
     });
+
+    lastmodified = resp.headers.get("Modified-Since");
+    if (resp.status === 304) return { lastmodified, rooms: false };
+    let json = await resp.json();
+    if (!resp.ok) throw `${resp.status} ${resp.statusText}: ${json.error}`;
+    else return { lastmodified, rooms: json };
 }
 
 // POST /api/login
-function login(userName) {
-    return fetch("/api/login", {
+async function login(userName) {
+    const resp = await fetch("/api/login", {
         method: "post",
         headers,
         body: JSON.stringify({ userName })
-    }).then(parseOrThrow);
+    });
+    return parseOrThrow(resp);
 }
 
 // POST /api/newroom
-function newRoom(user, roomName, secret = false) {
-    return fetch("/api/newroom", {
+async function newRoom(user, roomName, secret = false) {
+    const resp = await fetch("/api/newroom", {
         method: "post",
         headers: {
             ...headers,
             Authorization: JWT(user)
         },
         body: JSON.stringify({ roomName, secret })
-    }).then(parseOrThrow);
+    });
+    return parseOrThrow(resp);
 }
 
 // DELETE /api/deleteroom
-function deleteRoom(user, roomId) {
-    return fetch("/api/deleteroom", {
+async function deleteRoom(user, roomId) {
+    const resp = await fetch("/api/deleteroom", {
         method: "delete",
         headers: {
             ...headers,
             Authorization: JWT(user)
         },
         body: JSON.stringify({ roomId })
-    }).then(parseOrThrow);
+    });
+    return parseOrThrow(resp);
 }
 
 export { openRooms, login, newRoom, deleteRoom };
