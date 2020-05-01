d3d61d9140f49b6d88926583991401d6d66ded44
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2020-05-01 21:23:18 +0200

    Setup basic docs structure

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  title: 'Destroyer2',
+  description: 'A node real-time multiplayer battleship game',
+  base: '/destroyer2/',
+  evergreen: false,
+  themeConfig: {
+    repo: 'umcconnell/destroyer2',
+    docsDir: 'docs',
+
+    nav: [
+      { text: 'Guide', link: '/guide/' }
+    ],
+
+    editLinks: true,
+    editLinkText: 'Help us improve this page!',
+    sidebar: 'auto',
+    lastUpdated: true
+  }
+}
