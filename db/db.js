6c3376cb46671d7b42705923124b2992b4e3383d
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2020-05-09 19:44:53 +0200

    Add retry_strategy to redis clients
    When the connection to the redis database fails, the server
    crashes instantly without trying to reconnect. Adding a
    retry_strategy allows to retry establishing the connection without
    crashing the server in production. This feature is only enabled
    in production.

--- db/db.js
+++ db/db.js
@@ -1,11 +1,15 @@
 const redis = require("redis");
 const bluebird = require("bluebird");
 const logger = require("@helpers/logger");
+
+const retry_strategy = require("./retryStrategy");
+
 bluebird.promisifyAll(redis);
 
 let client = redis.createClient({
     password: process.env.DB_PASS,
-    url: process.env.DB_URL
+    url: process.env.DB_URL,
+    retry_strategy
 });
 
 if (process.env.AGGRESSIVE_CLEANUP) {
--- db/pubsub.js
+++ db/pubsub.js
@@ -1,8 +1,10 @@
 const redis = require("redis");
+const retry_strategy = require("./retryStrategy");
 
 let pub = redis.createClient({
         password: process.env.DB_PASS,
-        url: process.env.DB_URL
+        url: process.env.DB_URL,
+        retry_strategy
     }),
     sub = pub.duplicate();
 
