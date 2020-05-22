13071bc0da1cf3c40eb5c2bee256d00dea86dbba
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2020-05-22 22:03:18 +0200

    Add deployer help

---
+++ deploy/scripts/help.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Help menu
+echo "Commands:"
+echo "  help             Show this help"
+echo "  simple           Simple docker setup (node and redis)"
+echo "  reverse-proxy    Reverse-proxy setup (nginx, node and redis)"
+echo "  clean            Clean-up all unused docker images"
+echo "  stop             Stop all running docker containers"
+exit 0
