2354cdbe93f9fa98801591569def9a0faa83b5a4
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2020-05-22 22:04:56 +0200

    Add deployer simple deploy script

---
+++ deploy/scripts/simple.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "docker-compose -f deploy/simple/docker-compose.yml up -d"
+echo ""
+docker-compose -f deploy/simple/docker-compose.yml up -d
+echo ""
+echo "Done!"
+echo "Run docker ps to see running processes"
+echo "Run docker stop CONTAINER_ID to stop a container"
