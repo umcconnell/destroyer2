03964f7beb9e75a5299a12cddb534724673b7756
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2020-11-01 12:04:53 +0100

    Update joi dependency
    This commit replaces @hapi/joi with joi, as hapi has been dissolved
    and @hapi/joi will be deprecated soon.

--- models/validation.js
+++ models/validation.js
@@ -1,34 +1,58 @@
-let Joi = require("@hapi/joi");
+let Joi = require("joi");
 
 exports.userAuth = Joi.object()
     .keys({
-        userId: Joi.string().uuid().required(),
-        userName: Joi.string().max(20).required()
+        userId: Joi.string()
+            .uuid()
+            .required(),
+        userName: Joi.string()
+            .max(20)
+            .required()
     })
     .pattern(/./, Joi.any());
 
 exports.userName = Joi.object()
-    .keys({ userName: Joi.string().max(20).required() })
+    .keys({
+        userName: Joi.string()
+            .max(20)
+            .required()
+    })
     .pattern(/./, Joi.any());
 
 exports.userId = Joi.object()
-    .keys({ userId: Joi.string().uuid().required() })
+    .keys({
+        userId: Joi.string()
+            .uuid()
+            .required()
+    })
     .pattern(/./, Joi.any());
 
 exports.roomDetails = Joi.object()
     .keys({
-        roomName: Joi.string().max(20).required(),
-        roomId: Joi.string().uuid().required(),
+        roomName: Joi.string()
+            .max(20)
+            .required(),
+        roomId: Joi.string()
+            .uuid()
+            .required(),
         secret: Joi.bool()
     })
     .pattern(/./, Joi.any());
 
 exports.roomName = Joi.object()
-    .keys({ roomName: Joi.string().max(20).required() })
+    .keys({
+        roomName: Joi.string()
+            .max(20)
+            .required()
+    })
     .pattern(/./, Joi.any());
 
 exports.roomId = Joi.object()
-    .keys({ roomId: Joi.string().uuid().required() })
+    .keys({
+        roomId: Joi.string()
+            .uuid()
+            .required()
+    })
     .pattern(/./, Joi.any());
 
 exports.roomSecret = Joi.object()
