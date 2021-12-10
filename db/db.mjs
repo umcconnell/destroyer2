32c7d5a08e89b87345d16e851cc99c0220c30165
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2021-12-10 19:01:41 +0100

    Rewrite db connection setup
    Use the new node-redis package, including the new es6 import
    syntax. The new redis methods are async by default, so bluebird is
    no longer needed.
    node-redis uses a connection pool which conflicts with redis monitor
    mode in development, because monitor mode takes over the connection.
    As a workaround, a redis-cli instance in monitor mode is launched in
    the background and streams back stdin into the node process.

---
+++ db/db.mjs
@@ -0,0 +1,43 @@
+import { spawn } from "child_process";
+import { createClient } from "redis";
+
+import logger from "#helpers/logger";
+import { toBool } from "#helpers/utils";
+
+import reconnectStrategy from "./reconnectStrategy.mjs";
+
+const DB_URL = process.env.REDIS_PASS || process.env.DB_PASS;
+const DB_PWD = process.env.REDIS_PASS || process.env.DB_PASS;
+
+let client = createClient({
+    url: DB_URL,
+    password: DB_PWD,
+    socket: { reconnectStrategy }
+});
+
+await client.connect();
+
+if (toBool(process.env.AGGRESSIVE_CLEANUP)) {
+    await client.sendCommand(["CONFIG", "SET", "notify-keyspace-events", "Ex"]);
+}
+
+if (process.env.NODE_ENV !== "production") {
+    const monitor = spawn(
+        "redis-cli",
+        [
+            DB_URL ? `-u ${DB_URL}` : undefined,
+            DB_PWD ? `-a ${DB_PWD}` : undefined,
+            "monitor"
+        ].filter((el) => !!el)
+    );
+
+    monitor.stdout.on("data", (data) => {
+        logger.info(`MONITOR: ${data}`);
+    });
+
+    monitor.stderr.on("data", (data) => {
+        logger.error(`Monitor: ${data}`);
+    });
+}
+
+export default client;
