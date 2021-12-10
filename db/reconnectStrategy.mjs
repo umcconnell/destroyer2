2111250f4bba16a22f60fefb7cdc28406f3899e2
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2021-12-10 19:09:41 +0100

    Rewrite db reconnectStrategy

--- db/reconnectStrategy.mjs
+++ db/reconnectStrategy.mjs
@@ -1,19 +1,16 @@
-const logger = require("@helpers/logger");
+import logger from "#helpers/logger";
 
 const CONNECTION_RETRY = process.env.DB_CONNECTION_RETRY || 20;
 
-function retry_strategy(options) {
+export default function reconnectStrategy(options) {
     if (process.env.NODE_ENV !== "production") {
-        return undefined;
+        return new Error("DB connection retry not attempted (not production)");
     } else if (options.total_retry_time > 1000 * 60 * 60) {
-        return new Error("Database connection retry time exhausted");
+        return new Error("DB connection retry time exhausted");
     } else if (options.attempt > CONNECTION_RETRY) {
-        // End reconnecting with built in error
-        return undefined;
+        return new Error("DB connection attempts exhausted");
     } else if (options.attempt % 10 == 0) {
         logger.warn("Database connection failed! Retrying...");
     }
     return Math.min(options.attempt * 100, 3000);
 }
-
-module.exports = retry_strategy;
